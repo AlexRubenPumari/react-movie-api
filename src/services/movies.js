8c54ddede3f6bd69b180d31ff075b907259f408f
Alex Pumari <alexpumari0@gmail.com>
2025-06-05 19:23:26 -0300

    Fix movies service

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,6 +1,9 @@
 import { LANG_QUERY } from '../config/constants'
+import { getYear } from '../logic/utils'
 
-export default function getMovies ({ query, currentPage = 1 }) {
+export default function getMovies({ query, currentPage = 1 }) {
+  if (query === '') return null
+
   const URL = query
     ? `https://api.themoviedb.org/3/search/movie?api_key=ad42e8664bda3dbd31a5a27424023da8&query=${query}&include_adult=true&language=${LANG_QUERY}&page=${currentPage}`
     : `https://api.themoviedb.org/3/movie/popular?api_key=ad42e8664bda3dbd31a5a27424023da8&language=${LANG_QUERY}&page=${currentPage}`
@@ -11,5 +14,13 @@ export default function getMovies ({ query, currentPage = 1 }) {
         return response.json()
       }
     })
-    .then(data => data.results)
+    .then(data =>
+      data.results.map(({ title, overview, release_date, poster_path, id }) => ({
+        id,
+        title,
+        description: overview,
+        year: getYear(release_date),
+        posterPath: poster_path
+      }))
+    )
 }
