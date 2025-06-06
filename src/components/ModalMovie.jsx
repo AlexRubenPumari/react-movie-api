284a24295dbb7ce22e2bea9a79e4814e2fc706fa
Alex Pumari <alexpumari0@gmail.com>
2025-06-06 00:57:11 -0300

    Fix modal styles

--- src/components/ModalMovie.jsx
+++ src/components/ModalMovie.jsx
@@ -5,7 +5,7 @@ export default function ModalMovie ({ movie, onClose }) {
   const { title, description, posterPath, year } = movie
   return (
     <Modal className='ModalMovie' onClose={onClose}>
-      <h2>{`${title} (${year})`}</h2>
+      <h2 className='ModalMovie__title'>{`${title} (${year})`}</h2>
       <Poster pathImg={posterPath} />
       <p className='ModalMovie__desc Scrollbar'>{description}</p>
     </Modal>
